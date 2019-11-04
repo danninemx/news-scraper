7fc81de7dce4c54a133bc8609b7bfff50342b953
Danny Kim <dudkny@gmail.com>
2019-11-04 17:12:31 -0600

    Cleaned up comments

--- controllers/headline.js
+++ controllers/headline.js
@@ -6,7 +6,6 @@ const cheerio = require("cheerio");
 const db = require("../models");
 
 // Initialize Express
-//const app = express();
 const express = require("express");
 const app = express();
 
@@ -16,7 +15,7 @@ const mongoose = require("mongoose");
 module.exports = function (app) {
 
   // Route for getting all unsaved articles from the db
-  //this is for the root and uses index.handlebars
+  // This is for the root and uses index.handlebars
   app.get("/", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({ saved: false }, function (err, result) {
@@ -31,9 +30,8 @@ module.exports = function (app) {
     });
   });
 
-  //this grabs id of article being saved and updates the saved from false to true
+  // This grabs id of article being saved and updates the saved from false to true
   app.put("/savedarticles/:id", function (req, res) {
-
     db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true })
       .then(function (result) {
         console.log("this savedarticle is working");
@@ -46,7 +44,7 @@ module.exports = function (app) {
       });
   });
 
-  //this grabs id of article being unsaved and updates the saved from true to false
+  // This grabs id of article being unsaved and updates the saved from true to false
   app.put("/unsavedarticles/:id", function (req, res) {
 
     db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: false })
@@ -61,17 +59,17 @@ module.exports = function (app) {
       });
   });
 
-  //route for getting all the saved articles
-  //this is for the /saved path and uses saved.handlebars
+  // Route for getting all of the saved articles
+  // This is for the /saved path and uses saved.handlebars
   app.get("/saved", function (req, res) {
-    //Query: in our database, go to the articles collection, 
-    //then "find" every article that is saved (has a saved value of true);
+    // Query: in our database, go to the articles collection, 
+    // Then "find" every article that is saved (has a saved value of true);
     db.Article.find({ saved: true }, function (error, result) {
-      //Log any errors if the server encounters one.
+      // Log any errors if the server encounters one.
       if (error) {
         console.log("Error in getting saved articles: " + error);
       }
-      //Otherwise, send the result of this query to the browser.
+      // Otherwise, send the result of this query to the browser.
       else {
         //res.json(result);
         res.render("saved", {
@@ -81,7 +79,7 @@ module.exports = function (app) {
     });
   });
 
-  // // Route for getting all articles from the db
+  // // Route for getting all articles from the db. Currently no need on site.
   // app.get("/all", function (req, res) {
   //   // Grab every document in the Articles collection
   //   db.Article.find({ saved: false }, function (err, result) {
@@ -96,7 +94,7 @@ module.exports = function (app) {
   //   });
   // });
 
-  //this grabs id of article and deletes it from the database
+  // This grabs id of article and deletes it from the database
   app.delete("/deletearticles/:id", function (req, res) {
 
     db.Article.findOneAndRemove({ _id: req.params.id })
